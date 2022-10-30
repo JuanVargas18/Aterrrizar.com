2243eae232e7a438aa6e79f7b33fe4a251264429
Juan Vargas <juan18031205@gmail.com>
2022-10-30 16:19:17 -0300

    agregamos los estados

---
+++ aterrizar.com.py
@@ -0,0 +1,20 @@
+class Asiento:
+    def __init__(self):
+        self.esta_vendido = True
+        
+    pass 
+
+
+
+class Cliente:
+    pass
+
+
+class Libre:
+    pass
+
+class Vendido:
+    pass
+
+class Reservado:
+    pass
